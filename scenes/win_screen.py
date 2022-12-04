0401a2c7cbc611270af7c01a804a814acf2101fb
Ahmed Asi <95191621+AchoDev@users.noreply.github.com>
2022-12-04 13:29:22 +0100

    Update win_screen.py

--- scenes/win_screen.py
+++ scenes/win_screen.py
@@ -16,14 +16,14 @@ def load_scene(winner):
             case "enemy":
                 main.WIN.fill(COL.red.value)
 
-                text = Text(0, 0, COL.black.value, 70, "oh NO! enemy won?! why you so bad man????")
+                text = Text(0, 0, COL.black.value, 70, "oh NO! YOU LOST!")
                 text.place_center(main.WIN) # okan hat mir bei diesem Projekt geholfen!
                 
                 text.draw(main.WIN)
             case "player":
                 main.WIN.fill(COL.blue.value)
 
-                text = Text(0, 0, COL.black.value, 60, "WOW omg !!! you won you so good now you can die peacefully")
+                text = Text(0, 0, COL.black.value, 60, "WOW omg !!! YOU WON!")
                 text.place_center(main.WIN)
 
                 text.draw(main.WIN)
